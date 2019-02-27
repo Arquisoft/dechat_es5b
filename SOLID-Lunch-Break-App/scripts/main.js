8ed413980674c668bb652c3d1b8cd12b89ba94d8
Cristina Martin Rey <martinreycristina@yahoo.es>
2019-02-27 23:30:47 +0100

    Added a second version of SOLID-LunchBreak

--- SOLID-Lunch-Break-App/scripts/main.js
+++ SOLID-Lunch-Break-App/scripts/main.js
@@ -1,4 +1,8 @@
 const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+const fileClient = SolidFileClient;
+
+let userDataUrl = 'https://martinreycristina.solid.community/public/prueba2.ttl';
+
 
 // Log the user in and out on click
 const popupUri = 'popup.html';
@@ -30,6 +34,16 @@ $('#view').click(async function loadProfile() {
     // Display their details
     const fullName = store.any($rdf.sym(person), FOAF('name'));
     $('#fullName').text(fullName && fullName.value);
+	
+	const message = {
+		message: "hola samuel"
+	};
+	
+	const cris = await solid.auth.currentSession();
+	
+	console.log(cris);
+	
+	solid.auth.fetch('https://martinreycristina.solid.community/public/prueba').then(console.log);
 
     // Display their friends
     const friends = store.each($rdf.sym(person), FOAF('knows'));
@@ -42,5 +56,73 @@ $('#view').click(async function loadProfile() {
                 $('<a>').text(fullName && fullName.value || friend.value)
                     .click(() => $('#profile').val(friend.value))
                     .click(loadProfile)));
+		$('#friends').append(
+                $('<a>').text('Send Message')
+					.click(()=> createFolderWithMessages()));
     });
 });
+
+function createFolderWithMessages(){
+	// const gameUrl = generateUniqueUrlForResource(userDataUrl);
+	// executeSPARQLUpdateForUser(executeSPARQLUpdateForUser(userDataUrl, 
+		// `INSERT DATA {<${gameUrl}> <hola> <quetal>}`));
+	fileClient.createFolder(userDataUrl).then(success => {
+	  console.log(`Created folder ${url}.`);
+	}, err => console.log(err) );
+}
+
+/**
+   * This method executes an SPARQL update on a file.
+   * @param url: the url of the file that needs to be updated.
+   * @param query: the SPARQL update query that needs to be executed.
+   * @returns {Promise}: the promise from auth.fetch().
+   */
+ function executeSPARQLUpdateForUser(url, query) {
+    return this.fetch(url, {
+      method: 'PATCH',
+      body: query,
+      headers: {
+        'Content-Type': 'application/sparql-update'
+      }
+    });
+  }
+  
+  /**
+   * This method checks if the current user has write access to a file.
+   * @param url: the url of the file to check.
+   * @param dataSync: the DataSync object to do access.
+   * @returns {Promise<boolean>}: a promise that resolves with true if the user has write access, else false.
+   */
+  async function writePermission(url, dataSync) {
+    // TODO We should probably check the ACL of the parent folder to see if we can write if the file doesn't exist and
+    // if the file exists, we check the ACL of the file.
+    const response = await dataSync.executeSPARQLUpdateForUser(url, 'INSERT DATA {}');
+    return response.status === 200;
+  }
+  
+  /**
+   * This method generates a unique url for a resource based on a given base url.
+   * @param baseurl: the base url for the url of the resource.
+   * @returns {Promise<string>}: a promise that resolves with a unique url.
+   */
+  async function generateUniqueUrlForResource(baseurl) {
+	  let count =1;
+    let url = baseurl + '#' + '1234';
+
+    try {
+      let d = this.getObjectFromPredicateForResource(url, namespaces.rdf + 'type');
+
+      // We assume that if this url doesn't have a type, the url is unused.
+      // Ok, this is not the most fail-safe thing.
+      // TODO: check if there are any triples at all.
+      while (d) {
+        url = baseurl + '#' + (count+1);
+        d = await this.getObjectFromPredicateForResource(url, namespaces.rdf + 'type');
+      }
+    } catch (e) {
+      // this means that response of data[url] returns a 404
+      // TODO might be called when you have no access, should check
+    } finally {
+      return url;
+    }
+  }
