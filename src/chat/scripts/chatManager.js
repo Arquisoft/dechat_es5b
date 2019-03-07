5c50ec7af6a26ea256d17ddeb36c1b1f3a81321c
Cristina Martin Rey <martinreycristina@yahoo.es>
2019-03-07 18:41:54 +0100

    The most significant methods of the solid-file-client library have been introduced to the chatManager.js script.

--- src/chat/scripts/chatManager.js
+++ src/chat/scripts/chatManager.js
@@ -1,25 +1,7 @@
 const fileClient = require('solid-file-client')
 
-function createChatFolder(url) {
-    fileClient.createFolder(url).then(success => {
-        console.log(`Created folder ${url}.`);
-      }, err => console.log(err) );
-}
-
-function writeMessage(url,content){
-    fileClient.createFile(url,content,"text").then( fileCreated => {
-        console.log(`Created file ${fileCreated}.`);
-      }, err => console.log(err) );
-}
-
-function readFolder(url){
-    fileClient.readFolder(url).then(folder => {
-        console.log(`Read ${folder.name}, it has ${folder.files.length} files.`);
-      }, err => console.log(err) );
-}
-
 function sendMessage(URI,user,text){
-    var solidChat=URI+"public/SolidChat-5/";
+    var solidChat=URI+"public/SolidChat/";
     var folder= solidChat+user+"/";
 
     //New Solid-Chat folder
@@ -53,8 +35,8 @@ function sendMessage(URI,user,text){
 }
 
 //TO-DO
-function readMessage(){
-
+function receiveMessage(){
+	
 }
 
 module.exports = {
@@ -63,4 +45,49 @@ module.exports = {
     readMessage: readMessage,
     sendMessage: sendMessage,
     readFolder: readFolder
+}
+
+function createChatFolder(url) {
+    fileClient.createFolder(url).then(success => {
+        console.log(`Created folder ${url}.`);
+      }, err => console.log(err) );
+}
+
+//We have to know about what returns the method fileClient.readFolder(url)
+function readFolder(url){
+    fileClient.readFolder(url).then(folder => {
+        console.log(`Read ${folder.name}, it has ${folder.files.length} files.`);
+      }, err => console.log(err) );
+}
+
+function deleteFolder(url){
+	fileClient.deleteFolder(url).then(success => {
+	  console.log(`Deleted ${url}.`);
+	}, err => console.log(err) );
+}
+
+function writeMessage(url,content){
+    fileClient.createFile(url,content,"txt").then( fileCreated => {
+        console.log(`Created file ${fileCreated}.`);
+      }, err => console.log(err) );
+}
+
+//We have to know about what returns the method fileClient.readFile(url)
+function readMessage(url){
+	fileClient.readFile(url).then(  body => {
+	  console.log(`File	content is : ${body}.`);
+	}, err => console.log(err) );
+}
+
+//I've put this method here in case we end up using it.
+function updateMessage(url){
+	fileClient.updateFile( url, newContent, contentType ).then( success => {
+		console.log( `Updated ${url}.`)
+	}, err => console.log(err) );
+}
+
+function deleteMessage(url){
+	fileClient.deleteFile(url).then(success => {
+	  console.log(`Deleted ${url}.`);
+	}, err => console.log(err) );
 }
