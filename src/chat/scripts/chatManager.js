f93d32f012de073dd9b7b7571a6dd8bdaa2e2077
DiegoFernMart <diegofernmart@gmail.com>
2019-03-07 10:40:38 +0100

    Updated creation folder method

--- src/chat/scripts/chatManager.js
+++ src/chat/scripts/chatManager.js
@@ -12,23 +12,44 @@ function writeMessage(url,content){
       }, err => console.log(err) );
 }
 
-
-//
-function checkFolderExicts(URI){
-
+function readFolder(url){
+    fileClient.readFolder(url).then(folder => {
+        console.log(`Read ${folder.name}, it has ${folder.files.length} files.`);
+      }, err => console.log(err) );
 }
 
 function sendMessage(URI,user,text){
-    //already created Folder:
-    //this.checkFolderExicts(URI);
-    //New Folder:
-    console.log("Creating folder: "+ URI+"public/SolidChat/"+user+"/" +"....");
-    this.createChatFolder(URI+"public/SolidChat/"+user+"/");
+    var solidChat=URI+"public/SolidChat-5/";
+    var folder= solidChat+user+"/";
+
+    //New Solid-Chat folder
+    try{
+        var err = this.readFolder(solidChat);
+        if(!err){
+            console.log("Solid-chat folder doesnt exist");
+            throw("error")
+        }
+    }catch(error){
+        this.createChatFolder(solidChat);
+        console.log("Solid-chat folder created");
+    }
+    
+    //IF folder doesnt exist: create new user folder
+    try{
+        var err2=this.readFolder(folder);
+        if(!err2){
+            console.log("Folder doesnt exist");
+            throw("error")
+        }
+    }catch(error){
+         //New Folder:
+         this.createChatFolder(folder);
+         console.log('User folder created');
+    }
 
     //WritingMessage
     console.log("Writting message..."+text);
-    this.writeMessage(URI+"public/SolidChat/"+user+"/", text+".txt");
-
+    this.writeMessage(folder, text);
 }
 
 //TO-DO
@@ -40,5 +61,6 @@ module.exports = {
     createChatFolder: createChatFolder,
     writeMessage: writeMessage,
     readMessage: readMessage,
-    sendMessage: sendMessage
+    sendMessage: sendMessage,
+    readFolder: readFolder
 }
