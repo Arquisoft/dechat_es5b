1ffd70f8a9ae6c49748e32180b18be935bf485a4
WenjiMa <41855272+wenlormz@users.noreply.github.com>
2019-03-12 23:10:10 +0100

    Add files via upload

---
+++ test/LogInManagerTest.js
@@ -0,0 +1,18 @@
+import { describe, it } from 'mocha';
+import { assert, expect } from 'chai';
+var flogin = require('../src/chat/scripts/LogInManager.js').login;
+var flogout = require('../src/chat/scripts/LogInManager.js').logout;
+
+
+ describe('LogInManagerTest', function () {
+   it('Testing Login', async function () {
+     let r =  await flogin();
+     assert.equal(r, true)
+  });
+
+  it('Testing Logout', async function () {
+    let r = await flogout();
+    assert.equal(r, true)
+  });
+
+});
