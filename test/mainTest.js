33c2754ae9e6d4607958460fc12de0ae4e8b608b
Cristina Martin Rey <martinreycristina@yahoo.es>
2019-03-13 23:09:57 +0100

    Deleted mainTest.js

--- test/mainTest.js
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'mocha'
-import { assert, expect } from 'chai'
-
-describe('math', function () {
-  it('1 plus 1 is not 1', function () {
-    assert.equal(1+1, 2)
-  })
-})
