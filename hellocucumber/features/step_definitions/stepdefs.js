16f198fbf0464c060877814019030e891223ae0d
uo258510 <uo258510@uniovi.es>
2019-03-31 15:30:37 +0200

    send_message feature implemented

--- hellocucumber/features/step_definitions/stepdefs.js
+++ hellocucumber/features/step_definitions/stepdefs.js
@@ -1,5 +1,8 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
+const chatManager = require("../../../src/chat/scripts/chatManager.js");
+const fc = require("solid-file-client"); // <<<<<--------------------------------
+const OK = 1;							// <<<<<--------------------------------
 
 function isItFriday(today) {
   if (today === "Friday") {
@@ -19,4 +22,22 @@ When('I ask whether it\'s Friday yet', function () {
 
 Then('I should be told {string}', function (expectedAnswer) {
   assert.equal(this.actualAnswer, expectedAnswer);
+});
+
+Given('I\'m using the chat', function() {
+	chatManager.INFO.user="jandrolaviana";
+	chatManager.INFO.userName="jandrolaviana";
+	chatManager.INFO.userURI="https://jandrolaviana.solid.community";
+	chatManager.INFO.receiver="pruebaes5b";
+	chatManager.INFO.recieverName="pruebaes5b";
+	chatManager.INFO.recieverURI="https://pruebaes5b.solid.community";
+});
+
+When('I send a message {string} to a friend', async function(message) {
+	chatManager.sendMessage = function() { return message; }
+	this.reply = await chatManager.sendMessage(message);
+});
+
+Then('My friend gets the message {string}', function(message) {
+	assert.equal(this.reply, message); 
 });
