a96ce969bc876eb75cd581566ebcd87243ea006e
uo258510 <uo258510@uniovi.es>
2019-04-02 20:44:05 +0200

    Tryding deployment another time

---
+++ docs/scripts/LogInManager.js
@@ -0,0 +1,21 @@
+var podUtils = require('./podUtilities.js');
+
+async function login (credentials) {
+    podUtils.login(credentials);
+    $('#login').addClass('d-none');
+    $('#logout').removeClass('d-none');
+    $('#chatRef').removeClass('d-none');
+}
+
+async function logout () {
+    podUtils.logout();
+    $('#login').removeClass('d-none');
+    $('#logout').addClass('d-none');
+    $('#chatRef').addClass('d-none');
+}
+
+async function cosroro(){}
+module.exports = {
+    login: login,
+    logout: logout
+}
