a7b0ae620e206721914a6ebe03e5064beee4d21b
WenjiMa <41855272+wenlormz@users.noreply.github.com>
2019-04-08 15:03:52 +0200

    Adding chinese

--- src/scripts/i18n/two-people-chat.lang.js
+++ src/scripts/i18n/two-people-chat.lang.js
@@ -34,6 +34,21 @@ function setLanguage(locale){
 				"": ""
 			}
 			break;
+		case "zh-CN":
+			lang = {
+				"title": "DeChat ES5B - 聊天室",
+				"dropdownUser": "用户资料",
+				"profile": "转到您的SOLID个人资料",
+				"navBarLogout": "登出",
+				"langSelector": "语言",
+				"h5friends": "好友列表",
+				"h5friendsDescription": "请在开始聊天之前选择一位朋友",
+				"h1title": "欢迎来到您的分散聊天室",
+				"sendButton": "发送",
+				"": "",
+				"": ""
+			}
+			break;
 		default:
 			console.error("No language set");
 	}
