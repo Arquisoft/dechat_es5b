5af4da52b44737ec94ed23e36b07b300611e639f
WenjiMa <41855272+wenlormz@users.noreply.github.com>
2019-04-08 15:03:00 +0200

    Adding chinese

--- src/scripts/i18n/index.lang.js
+++ src/scripts/i18n/index.lang.js
@@ -50,6 +50,29 @@ function setLanguage(locale){
 				"idpLogin": "Iniciar sesión"
 			}
 			break;
+		case "zh-CN":
+			lang = {
+				"title": "DeChat ES5B - 您的分散聊天室",
+				"about": "关于聊天室",
+				"langSelector": "语言",
+				"desktopApp": "下载桌面 App",
+				"h1header": "欢迎来到您的分散聊天室",
+				"h2header": "使用您的 Solid POD 登录",
+				"logInBtn": "登录",
+				"logOutBtn": "登出",
+				"chatRef": "进入聊天室",
+				"h3header": "开始聊天",
+				"desc1": "只需使用您的 Solid 社区帐户登录即可开始聊天。",
+				"desc2": "您必须拥有可靠的社区帐户中的朋友才能聊天。",
+				"h3header2": "怎么运作",
+				"desc3": "我们的 deChat 是一个分散的聊天网站，建立在 Solid 之上。",
+				"desc4": "有关聊天的所有个人数据都存储在您的 POD 中。",
+				"modalIDPTitle": "选择您的身份提供商 (IDP) 或者 WebID",
+				"modalIDPDescription": "如果要使用 Solid Community Server 登录, 将此字段留空并单击 \"使用 Solid Community 登录\"",
+				"solidLogin": "使用 Solid Community 登录",
+				"idpLogin": "登录"
+			}
+			break;
 		default:
 			console.error("No language set");
 	}
