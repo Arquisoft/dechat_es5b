8ef4e3861cfcf7b63f78b1395ebc66be9b2fba24
Carlos <28845529+SrCharlystar@users.noreply.github.com>
2019-04-08 13:13:52 +0200

    Added dropdown to index

--- src/scripts/i18n/index.lang.js
+++ src/scripts/i18n/index.lang.js
@@ -8,13 +8,14 @@ function setLoginLanguage(locale){
 			lang = {
 			  "title": "DeChat ES5B - Your decentralized chat",
 			  "about": "About the chat",
+			  "langSelector": "Language",
 			  "desktopApp": "Download desktop app",
 			  "h1header": "Welcome to our descentralized chat",
 			  "h2header": "Log in with your Solid POD!",
 			  "logInBtn": "Log in",
 			  "h3header": "Start chatting",
-			  "": "",
-			  "": "",
+			  "logOutBtn": "Log out",
+			  "chatRef": "Enter to chat",
 			  "": "",
 			  "": "",
 			  "": ""
@@ -24,13 +25,14 @@ function setLoginLanguage(locale){
 			lang = {
 			  "title": "DeChat ES5B - Chatea con tus amigos de forma descentralizada",
 			  "about": "Acerca de",
+			  "langSelector": "Idioma",
 			  "desktopApp": "Descarga la aplicación",
 			  "h1header": "Bienvenido a nuestro chat descentralizado",
 			  "h2header": "Inicia sesión con tu POD de SOLID",
 			  "logInBtn": "Inicia sesión",
 			  "h3header": "Chatea ahora",
-			  "": "",
-			  "": "",
+			  "logOutBtn": "Cerrar sesión",
+			  "chatRef": "Enter to chat",
 			  "": "",
 			  "": "",
 			  "": ""
@@ -42,11 +44,14 @@ function setLoginLanguage(locale){
 	//Set navbar phrases
 	document.title = lang.title;
 	$("#about").text(lang.about);
+	$("#langSelector").text(lang.langSelector);
 	$("#desktopApp").text(lang.desktopApp);
 	$("#h1header").text(lang.h1header);
 	$("#h2header").text(lang.h2header);
 	$("#logInBtn").text(lang.logInBtn);
 	$("#h3header").text(lang.h3header);
+	$("#logOutBtn").text(lang.logOutBtn);
+	$("#chatRef").text(lang.chatRef);
 }
 
 setLoginLanguage("en_US");
