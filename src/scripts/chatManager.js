5edc5aceb30549da2f7abc986c4ef2c641e1a6f9
antunaalonso <uo258510@uniovi.es>
2019-04-29 12:16:03 +0200

    Deleting tolog

--- src/scripts/chatManager.js
+++ src/scripts/chatManager.js
@@ -37,90 +37,65 @@ async function sendMessage(text) {
     var filename = folder + "chatld.jsonld";
 
     //WritingMessage
-    if (ToLog)
-        console.log("Writting message: " + text);
-    if (ToLog)
-        console.log("Check user:" + INFO.receiverName + " chat file")
     try {
         var err3 = await podUtils.readFile(filename);
         if (!err3) {
-            if (ToLog)
-                console.log("Chat file doesnt exist");
-            throw ("error")
+            throw ("error");
         }
 
         await podUtils.deleteFile(filename);
 
         var chat = JSON.parse(err3);
-        var message={
+        var message = {
             "@Type": "message",
             "sender": INFO.userURI,
             "dateSent": new Date().getTime(),
             "text": text
-            };
+        };
         chat.messages.push(message);
         jsonString = JSON.stringify(chat);
 
-        ret = await podUtils.writeMsgJsonld( folder + "chatld", jsonString, ToLog);
+        ret = await podUtils.writeMsgJsonld(folder + "chatld", jsonString, ToLog);
         if (notify)
             await notiMan.writeNotification(INFO.receiverURI, INFO.user);
     } catch (error) {
-
         //IF folder doesnt exist: create new user folder
-        if (ToLog)
-            console.log("Check user:" + INFO.receiverName + " folder")
         try {
             var err2 = await podUtils.readFolder(folder, ToLog);
             if (!err2) {
-                if (ToLog)
-                    console.log("Folder doesnt exist");
-                throw ("error")
+                throw ("error");
             }
         } catch (error) {
-
             //Check Folder SolidChat
-            if (ToLog)
-                console.log("Check SolidChat Exist")
             try {
                 var err = await podUtils.readFolder(solidChat, ToLog);
                 if (!err) {
-                    if (ToLog)
-                        console.log("Solid-chat folder doesnt exist");
-                    throw ("error")
+                    throw ("error");
                 }
             } catch (error) {
                 //New Solid-Chat folder
                 await podUtils.createFolder(solidChat, ToLog);
-                if (ToLog)
-                    console.log("Solid-chat folder created");
             }
 
             //New Folder:
             await podUtils.createFolder(folder, ToLog);
-            if (ToLog)
-                console.log('User folder created');
-
         }
-        if (ToLog)
-            console.log("Creating chat file");
-        
+
         //chat is the full chat component in jsonld
-        var chat={
+        var chat = {
             "@context": "http://schema.org/",
             "@type": "Conversation",
-            "messages":[
-                {
+            "messages": [{
                 "@Type": "message",
                 "sender": INFO.userURI,
                 "dateSent": new Date().getTime(),
                 "text": text
-                }
-            ]
+            }]
         };
 
         jsonString = JSON.stringify(chat);
 
-        ret = await podUtils.writeMsgJsonld( folder + "chatld", jsonString, ToLog);
+        ret = await podUtils.writeMsgJsonld(folder + "chatld", jsonString, ToLog);
         if (notify)
             await notiMan.writeNotification(INFO.receiverURI, INFO.user);
 
@@ -129,9 +104,6 @@ async function sendMessage(text) {
 }
 
 async function receiveMessages() {
-    if (ToLog)
-        console.log("ReceivingMessages")
-
     //Define folders name
     var uFolder = INFO.userURI + "public/SolidChat/" + INFO.receiverName.trim().replace(/ /g, "-") + "/";
     var rFolder = INFO.receiverURI + "public/SolidChat/" + INFO.userName.trim().replace(/ /g, "-") + "/";
@@ -145,38 +117,34 @@ async function receiveMessages() {
     userMessages = await podUtils.readFile(uFile, ToLog);
     receiveMessages = await podUtils.readFile(rFile, ToLog);
     if (!userMessages) {
-        if (ToLog)
-            console.log("User chat file doesnt exist");
         //await podUtils.createFile(uFile, JSON.stringify(dict), ToLog);
         userMessages = "[]";
     }
 
     if (!receiveMessages) {
-        if (ToLog)
-            console.log("Friend chat file doesnt exist");
         //await podUtils.createFile(rFile, JSON.stringify(dict), ToLog);
         receiveMessages = "[]";
     }
 
     var uParsed = JSON.parse(userMessages).messages;
     var rParsed = JSON.parse(receiveMessages).messages;
-    if(uParsed){
-    uParsed.forEach(element => {
-        var date = new Date(Number(element.dateSent));
-        var strDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " +
-            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-        dict.push(new message("<div class=\"containerChatDarker\"><p id=\"noMarginMessge\">" + element.text + "</p><p id=\"username\">" + INFO.userName + " (you) " + strDate + "</p></div>", date));
-    });
+    if (uParsed) {
+        uParsed.forEach(element => {
+            var date = new Date(Number(element.dateSent));
+            var strDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " +
+                date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+            dict.push(new message("<div class=\"containerChatDarker\"><p id=\"noMarginMessge\">" + element.text + "</p><p id=\"username\">" + INFO.userName + " (you) " + strDate + "</p></div>", date));
+        });
     }
-    if(rParsed){
-    rParsed.forEach(element => {
-        var date = new Date(Number(element.dateSent));
-        var strDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " +
-            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-        dict.push(new message("<div class=\"containerChat\"><p id=\"noMarginMessge\">" + element.text + "</p><p id=\"username\">" + INFO.receiverName + " " + strDate + "</p></div>", date));
-    });
+    if (rParsed) {
+        rParsed.forEach(element => {
+            var date = new Date(Number(element.dateSent));
+            var strDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " +
+                date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+            dict.push(new message("<div class=\"containerChat\"><p id=\"noMarginMessge\">" + element.text + "</p><p id=\"username\">" + INFO.receiverName + " " + strDate + "</p></div>", date));
+        });
     }
-    dict.sort(function (a, b) {
+    dict.sort(function(a, b) {
         return a.date > b.date ? 1 : a.date < b.date ? -1 : 0;
     });
 
