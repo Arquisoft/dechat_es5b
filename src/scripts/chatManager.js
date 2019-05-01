471f10ef0e16cda8409d1d384ef391d8485c8e18
uo258014 <ignacieiro@gmail.com>
2019-05-01 18:13:08 +0200

    Scroll bar fix

--- src/scripts/chatManager.js
+++ src/scripts/chatManager.js
@@ -270,7 +270,7 @@ async function receiveMessages() {
     dict.forEach((n) => {
         MESSAGES.toShow.push(n.text)
     });
-    MESSAGES.toShow = MESSAGES.toShow.slice(-10);
+    //MESSAGES.toShow = MESSAGES.toShow.slice(-10);
 
     return MESSAGES.toShow;
 }
@@ -478,7 +478,7 @@ async function receiveGroupMessages() {
     dict.forEach((n) => {
         MESSAGES.toShow.push(n.text)
     });
-    MESSAGES.toShow = MESSAGES.toShow.slice(-10);
+    //MESSAGES.toShow = MESSAGES.toShow.slice(-10);
     MESSAGES.toShow.unshift("<p> URL: " + folder + "</p>");
 
     return MESSAGES.toShow;
